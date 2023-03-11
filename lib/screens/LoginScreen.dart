fad9fdee5a4cd6c195952383068c4d0f003ff090
Hamza Rafique <103266161+hamzarafique45@users.noreply.github.com>
2023-03-12 00:17:39 +0500

    Working...

---
+++ lib/screens/LoginScreen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('General A P P')),
+    );
+  }
+}
