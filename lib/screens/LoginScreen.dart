f30fba099e8ec7920fab41118480c77569d0a1b3
Hamza Rafique <103266161+hamzarafique45@users.noreply.github.com>
2023-03-12 19:37:30 +0500

    stop work on this until customized textfield

--- lib/screens/LoginScreen.dart
+++ lib/screens/LoginScreen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:mini/homeScreen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,13 +11,61 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  // method to nevigate from login to Home Screen...
+  void userLogin() {
+    Navigator.pop(context);
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const HomeScreen(),
+        )
+        );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('General A P P')),
-      body: Center(
-        child: Text('UNDER DEVELOPMENT...'),
-        ),
+      backgroundColor: Colors.blueGrey[300],
+      body: SafeArea(
+        child: Center(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+
+
+                const SizedBox(height: 50),
+
+                // app logo...
+                Icon(
+                  Icons.lock,
+                  size: 100,
+                  color: Colors.blueGrey[900],
+                  ),
+
+                const SizedBox(height: 50),  
+
+                // welcome note of app
+                Text(
+                  'Welcom back to your G E N E R A L  A P P',
+                  style: TextStyle(
+                    color: Colors.blueGrey[700],
+                    fontSize: 14,
+                  ),
+                ),
+
+                const SizedBox(height: 50), 
+
+                // Username Customized Textfield...
+                // Password Customized Textfield...
+
+
+
+              ],
+            )
+            ),
+        )
+         ),
     );
   }
 }
