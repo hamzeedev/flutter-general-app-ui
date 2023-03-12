830ab2827d1f87fc5f5f2fda40de0fbcabe78708
Hamza Rafique <103266161+hamzarafique45@users.noreply.github.com>
2023-03-12 20:05:39 +0500

    custom textfield created

---
+++ lib/customs/customTextfield.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/widgets.dart';
+
+class CustomTextField extends StatelessWidget {
+
+
+  final        controller;
+  final String hintText;
+  final bool   obscureText; // For hiding password eg (*****)
+
+
+  const CustomTextField({
+    super.key,
+    required this.controller,
+    required this.hintText,
+    required this.obscureText,
+  });
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: TextField(
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+          enabledBorder: const OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.blueGrey),
+          ),
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(color: Colors.blueGrey.shade400),
+          ),
+          fillColor: Colors.blueGrey.shade100,
+          filled: true,
+          hintText: hintText,
+          hintStyle: TextStyle(color: Colors.blueGrey[500]),
+        ),
+      ),
+    );
+  }
+}
