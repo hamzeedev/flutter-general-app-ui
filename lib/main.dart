21d02a510bcf4718d57acdda6ba5189bd047855f
Hamza Rafique <103266161+hamzarafique45@users.noreply.github.com>
2023-03-12 16:13:19 +0500

    Working

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:mini/screens/LoginScreen.dart';
 
 void main() {
-  runApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
+    );
+  }
 }
