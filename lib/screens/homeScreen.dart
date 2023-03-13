a3e15845db2b1599001568cdc68ca809fe27ffcc
Hamza Rafique <103266161+hamzarafique45@users.noreply.github.com>
2023-03-13 22:55:06 +0500

    Start working on HomeScreen

--- lib/screens/homeScreen.dart
+++ lib/screens/homeScreen.dart
@@ -1,14 +1,9 @@
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
-class HomeScreen extends StatefulWidget {
+class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
-  @override
-  State<HomeScreen> createState() => _HomeScreenState();
-}
-
-class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return const Placeholder();
